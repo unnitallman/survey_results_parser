8e9d882711a7e2a3a0ad548b1b74a1a3e37d211e
Unni <unni.tallman@gmail.com>
2015-08-13 22:53:45 +0530

    changes made in controller/views and routes for removing the placeholders in the skeleton code and display real data instead

--- app/controllers/survey_results_controller.rb
+++ app/controllers/survey_results_controller.rb
@@ -1,2 +1,15 @@
 class SurveyResultsController < ApplicationController
+  def index
+    @csv_files = CsvFile.all.order('created_at desc')
+  end
+
+  def show
+    @csv_file = CsvFile.find(params[:id])
+    @themes   = @csv_file.themes
+  end
+
+  def create
+    CsvParser::Main.new(params[:file]).load
+    redirect_to survey_results_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'survey_results#index'
 
-  resources :survey_results, only: [:show, :index]
+  resources :survey_results, only: [:show, :index, :create]
 end
